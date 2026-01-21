7e2daf4a2dd80fbac1e6000296b4bc75975380da
GeFoRcE123123 <94235250+GeFoRcE123123@users.noreply.github.com>
2026-01-22 01:34:10 +0300

    Исправлены критические ошибки: CVE отображаются, дашборд и аналитика работают
    Исправления:
    - get_paginated теперь фильтрует только записи с CVE (cve IS NOT NULL AND cve != '')
    - Исправлена ошибка JSON в get_active_parsing_status (row[6] уже dict)
    - API /api/vulnerabilities теперь возвращает данные
    - Дашборд и аналитика активны

--- models/legacy_repositories.py
+++ models/legacy_repositories.py
@@ -1338,7 +1338,12 @@ class LegacyVulnerabilityRepository:
                     where_conditions.append("etc ILIKE %s")
                     params.append(f'%"{t}"%')
             
-            where_clause = " AND ".join(where_conditions) if where_conditions else "1=1"
+            # Всегда фильтруем только записи с CVE
+            base_condition = "cve IS NOT NULL AND cve != ''"
+            if where_conditions:
+                where_clause = f"{base_condition} AND " + " AND ".join(where_conditions)
+            else:
+                where_clause = base_condition
             logger.debug(f"🔍 [get_paginated] WHERE clause: {where_clause}, params={params}")
             
             # Подсчет общего количества
